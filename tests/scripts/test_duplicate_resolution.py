1b197fe88c9a581b6abb01b06787e36a22d9c638
Tatiana Gurbich <tgurbich@gmail.com>
2024-03-22 02:12:22 +0000

    Added unit test for duplicate resolution - WIP

---
+++ tests/scripts/test_duplicate_resolution.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+
+import copy
+import os
+import re
+import sys
+import unittest
+
+sys.path.append(os.path.sep.join(os.getcwd().split(os.path.sep)[:-2]))
+
+from postprocessing.duplicate_resolution import main
+
+DUMMY_REFERENCE = os.path.join(
+    os.path.sep.join(os.getcwd().split(os.path.sep)[:-1]),
+    "test_inputs",
+    "dummy_reference.gff",
+)
+TEST_INPUT = os.path.join(
+    os.path.sep.join(os.getcwd().split(os.path.sep)[:-1]),
+    "test_inputs",
+    "deduplication_script_test_input.gff",
+)
+
+
+class TestDuplicateResolution(unittest.TestCase):
+    def test_main(self):
+
+        result = main(DUMMY_REFERENCE, TEST_INPUT, "deduplication_test_outfile.gff", "uniformis")
+        os.remove("deduplication_test_outfile.gff")
